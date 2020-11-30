2532811eed557d570f8be06a865991e3882b5e52
pbroeker <broeker1986@gmail.com>
2020-11-30 14:57:16 +0100

    basic langing page with chat

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { captureRejectionSymbol } = require('events');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -18,28 +19,62 @@ const io = socketIo(server, {
   }
 });
 
-io.on('connection', (socket) => {
-  console.log (`New user connected: ${socket.id}`);
 
-  socket.on('join-room', (player) => {
-    socket.join(`room${player.room}`);
-    console.log(`Player ${player.name} joined room ${player.room}`);
-    socket.to(`room${player.room}`).emit('player-joined-a-room' ,`Player ${player.name} joined this room`);
-  })
+
+let users = [];
+let rooms = [
+  { name: 'room1', id: 1, players: [] },
+  { name: 'room2', id: 2, players: [] },
+  { name: 'room2', id: 3, players: [] },
+];
+
+io.on('connection', (socket) => { 
+  socket.emit('send-rooms', rooms);
+
+  socket.on('join-room', ({ player, roomNumber }) => {
+    console.log(`Player ${player.name} joined room ${roomNumber}`);
+    
+    let newRooms = [...rooms];
+    let oldPlayersInRoom = Array.from(rooms[roomNumber-1].players);
+    let newPlayersInRoom = oldPlayersInRoom.filter((oldPlayer) => oldPlayer.name !== player.name);
+    newPlayersInRoom.push(player);
+    newRooms[roomNumber-1].players = newPlayersInRoom;
+    socket.join(`room${roomNumber}`);
+    io.emit('send-rooms', newRooms);
+    io.in(`room${roomNumber}`).emit('player-joined-a-room' ,`Player ${player.name} joined this room`);
+  });
+
+  socket.on('leave-room', ({ player, roomNumber }) => {
+    console.log(`Player ${player.name} left room ${roomNumber}`);
+    
+    let newRooms = [...rooms];
+    let oldPlayersInRoom = Array.from(rooms[roomNumber-1].players);
+    let newPlayersInRoom = oldPlayersInRoom.filter((oldPlayer) => oldPlayer.name !== player.name);
+    newRooms[roomNumber-1].players = newPlayersInRoom;
+    socket.leave(`room${roomNumber}`);
+    io.emit('send-rooms', rooms);
+    io.in(`room${roomNumber}`).emit('player-left-a-room' ,`Player ${player.name} joined this room`);
+  });
 
   socket.on('disconnect', () => {
     console.log(`user disconnected: ${socket.id}`);
-  })
+  });
 
-  socket.on('send-user-message', (data) => {
-    console.log(`sending message ${data.message} from ${data.player.name} to ${data.player.room}.`);
-    io.to(`room${data.player.room}`).emit('user-chat-message', data);
-  })
+  socket.on('send-user-message', ({ player, message}) => {
+    console.log(`sending message ${message} from ${player.name} to ${player.designatedRoom}.`);
+    io.in(`room${player.designatedRoom}`).emit('user-chat-message', { player:player, message: message });
+  });
 
   socket.on('admin-action', (message) => {
-    io.emit('admin-calling', message);
-  })
+    io.emit('send-user-message', { player: { name: 'Admin' } ,message: message });
+  });
 
+  socket.on('update-players', (player) => {
+    const newPlayers = users.filter((oldPlayer) => oldPlayer.name !== player.name)
+    newPlayers.push(player);
+    users = newPlayers;
+    io.emit('update-state-players', users);
+  });
 })
 
 server.listen(PORT, () => {
