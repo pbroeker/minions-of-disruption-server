77f754808fc0b19baf9d459a22edae0fa17f49c5
pbroeker <broeker1986@gmail.com>
2020-12-09 22:15:27 +0100

    chat working

--- controllers/chat.socket.controller.js
+++ controllers/chat.socket.controller.js
@@ -1,10 +1,12 @@
 const sendUserMessage = (adminNamespace, io) => ({ user, message, channel }) => {
   console.log(`User: sending message ${message} from ${user.name} to ${channel}.`);
-  if (channel = 99) {
+  if (channel === 99) {
+    console.log('CHANELL 99: user has channel', user, channel);
     io.emit('send-user-message', { user:user, message: message, channel: channel });
     adminNamespace.emit('send-user-message', { user:user, message: message, channel: channel });
   } else {
-    io.sockets.in(`room${user.designatedRoom}`).emit('send-user-message', { user:user, message: message, channel: channel });
+    console.log('CHANELL USER: user has channel', user, channel);
+    io.to(`room${user.designatedRoom}`).emit('send-user-message', { user: user, message: message, channel: channel });
     adminNamespace.emit('send-user-message', { user: user, message: message, channel: channel });
   }
 };
