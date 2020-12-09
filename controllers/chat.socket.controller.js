07093e38baa4ccf70a429fb7e4f2fa40ddc1f018
pbroeker <broeker1986@gmail.com>
2020-12-09 20:10:38 +0100

    before playing

--- controllers/chat.socket.controller.js
+++ controllers/chat.socket.controller.js
@@ -1,11 +1,11 @@
-const sendUserMessage = (adminNamespace, io) => ({ user, message }) => {
-  console.log(`User: sending message ${message} from ${user.name} to ${user.designatedRoom}.`);
-  if (user.designatedRoom === 99) {
-    io.emit('send-user-message', { user:user, message: message });
-    adminNamespace.emit('send-user-message', { user:user, message: message });
+const sendUserMessage = (adminNamespace, io) => ({ user, message, channel }) => {
+  console.log(`User: sending message ${message} from ${user.name} to ${channel}.`);
+  if (channel = 99) {
+    io.emit('send-user-message', { user:user, message: message, channel: channel });
+    adminNamespace.emit('send-user-message', { user:user, message: message, channel: channel });
   } else {
-    io.sockets.in(`room${user.designatedRoom}`).emit('send-user-message', { user:user, message: message });
-    adminNamespace.emit('send-user-message', { user: user, message: message });
+    io.sockets.in(`room${user.designatedRoom}`).emit('send-user-message', { user:user, message: message, channel: channel });
+    adminNamespace.emit('send-user-message', { user: user, message: message, channel: channel });
   }
 };
 
