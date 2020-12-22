b33c1a10e355958f781a95a7e02631953ec96516
pbroeker <broeker1986@gmail.com>
2020-12-22 19:43:46 +0100

    Heroku

--- src/routes.ts
+++ src/routes.ts
@@ -16,7 +16,7 @@ import path from 'path';
 // });
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 router.get('/token/:token', checkToken);
