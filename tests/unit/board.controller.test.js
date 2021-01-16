534bfbdf45cbb4ae9f57c3339d2f2a81ee9f731a
pbroeker <broeker1986@gmail.com>
2021-01-16 23:26:52 +0100

    refactor: Splitting up of boardcontrollers to controllers and services

--- tests/unit/board.controller.test.js
+++ tests/unit/board.controller.test.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import { insertMany, findById, findByIdAndUpdate, findByTokenId } from '../../src/services/board.service';
 import { getBoards, createBoards, updateBoard, loadBoard } from '../../src/controllers/server/board.controller';
 import sinon from 'sinon';
 
@@ -9,15 +10,20 @@ describe('Board controller', () => {
     sinon.restore();
   });
 
-  describe('Error handling', () => {
-    it('should log the error on fail', () => {});
+  // describe('Error handling', () => {
+  //   it('should log the error on fail', () => {});
 
-    it('should send a status of 500', () => {});
+  //   it('should send a status of 500', () => {});
 
-    it('should send the error', () => {});
+  //   it('should send the error', () => {});
+  // });
+
+  describe('Sending correct status codes', () => {
+    it('create board sends statusCode 201 on success', () => {
+      const mockReq = { body: {} };
+      const mReply = {};
+    });
   });
-
-  describe('Sending correct status codes', () => {});
 
   describe('Transfering correct data to DB', () => {});
 
