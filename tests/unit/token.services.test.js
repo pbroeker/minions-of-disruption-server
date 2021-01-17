e0bceecee7321ef3d4f286134d9f23042f61ecaa
pbroeker <broeker1986@gmail.com>
2021-01-17 05:25:56 +0100

    add: Token.services.test

---
+++ tests/unit/token.services.test.js
@@ -0,0 +1,62 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { create, update, check, getAll } from '../../src/services/token.services';
+import mongoose from 'mongoose';
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe('tokenServices', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('create', () => {
+    it('create calls Token.create with correct arguments ', () => {
+      const createStub = sinon.stub(mongoose.Model, 'create');
+      create('english', 'commercial', 1234);
+
+      expect(createStub).to.have.been.calledOnce;
+      expect(createStub).to.have.been.calledWith({ language: 'english', game_version: 'commercial', code: 1234 });
+      expect(createStub).not.to.have.been.calledWith('test');
+    });
+  });
+
+  describe('update', () => {
+    it('update calls Token.update with correct arguments  ', () => {
+      const updateStub = sinon.stub(mongoose.Model, 'findOneAndUpdate');
+      update(1234, ['123', '1234', '12345']);
+
+      expect(updateStub).to.have.been.calledOnce;
+      expect(updateStub).to.have.been.calledWith(
+        { code: 1234 },
+        { $set: { boardIds: ['123', '1234', '12345'] } },
+        { new: true }
+      );
+      expect(updateStub).not.to.have.been.calledWith({ code: 0, boardIds: [] });
+    });
+  });
+
+  describe('check', () => {
+    it('check calls Token.findOne with correct arguments ', () => {
+      const findOneStub = sinon.stub(mongoose.Model, 'findOne');
+      check(1234);
+
+      expect(findOneStub).to.have.been.calledOnce;
+      expect(findOneStub).to.have.been.calledWith({ code: 1234 });
+      expect(findOneStub).not.to.have.been.calledWith(123);
+    });
+  });
+
+  describe('getAll', () => {
+    it('getAll calls Board.find ', () => {
+      const findStub = sinon.stub(mongoose.Model, 'find');
+      getAll();
+
+      expect(findStub).to.have.been.calledOnce;
+      expect(findStub).to.have.been.calledWith();
+      expect(findStub).not.to.have.been.calledWith(null);
+    });
+  });
+});
